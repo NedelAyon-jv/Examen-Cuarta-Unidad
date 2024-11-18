15f35291a0d76fd7e152b5bb639cc8a99d183d7f
Vecs <166658809+Vecs09@users.noreply.github.com>
2024-11-18 12:30:29 -0700

    Backend / Logout

--- app/AuthController.php
+++ app/AuthController.php
@@ -17,6 +17,14 @@ require_once "../config.php";
 				$authController->access($correo,$contrasena);
 
 			break; 
+
+			case 'logout':
+				
+				$authController = new AuthController();
+
+				$authController->logout();
+
+			break;
 		}
 	}
 
@@ -58,6 +66,50 @@ require_once "../config.php";
 				header("Location:" . BASE_PATH . "index.php");
 			}
 
+		}
+
+		public function logout()
+		{
+			if (isset($_SESSION['user_data']) && isset($_SESSION['user_data']->token)) {
+				$token = $_SESSION['user_data']->token;
+				$email = $_SESSION['user_data']->email;
+	
+				$curl = curl_init();
+	
+				curl_setopt_array($curl, array(
+					CURLOPT_URL => 'https://crud.jonathansoto.mx/api/logout',
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_ENCODING => '',
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 0,
+					CURLOPT_FOLLOWLOCATION => true,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => 'POST',
+					CURLOPT_POSTFIELDS => array('email' => $email),
+					CURLOPT_HTTPHEADER => array(
+						'Authorization: Bearer ' . $_SESSION['user_data']->token
+					),
+				));
+	
+				$response = curl_exec($curl);
+				curl_close($curl);
+	
+				$response = json_decode($response);
+	
+				if (isset($response->success) && $response->success) {
+					session_unset();
+					session_destroy();
+	
+					header("Location:" . BASE_PATH . "index.php");
+					exit;
+				} else {
+					header("Location:" . BASE_PATH . "home");
+					exit;
+				}
+			} else {
+				header("Location:" . BASE_PATH . "index.php");
+				exit;
+			}
 		}
 
 
