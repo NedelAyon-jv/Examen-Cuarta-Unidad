aa651b4ad7e77a5d6fd71319990c1ff595ce891b
NedelAyon-jv <tuola9@gmail.com>
2024-11-22 11:28:28 -0700

    Maquetado/modificaciones ordenes

--- views/Ordenes/ordenes.php
+++ views/Ordenes/ordenes.php
@@ -5,9 +5,10 @@
 include "../../config.php";
 
 ?>
- <head>
-   <?php include "../layouts/head.php" ?>
- </head>
+
+<head>
+    <?php include "../layouts/head.php" ?>
+</head>
 <!-- [Head] end -->
 <!-- [Body] Start -->
 
@@ -51,10 +52,11 @@ include "../../config.php";
             <!-- [ breadcrumb ] end -->
 
             <a class="btn btn-outline-secondary shadow-sm rounded-pill px-4" href="#">Agregar orden</a>
-        <br>
-        <br>
-      <!-- [ Main Content ] start -->
-      <div class=" row">
+            <br>
+            <br>
+            <!-- [ Main Content ] start -->
+            <div class=" row">
+
                 <!-- [ sample-page ] start -->
                 <div class="col-sm-12">
                     <div class="card table-card">
@@ -69,6 +71,7 @@ include "../../config.php";
 
                                 <!-- Tabla de productos -->
                                 <div class="table-responsive">
+
                                     <table class="table table-hover tbl-product" id="pc-dt-simple">
                                         <thead>
                                             <!-- Filtro por tipo de catálogo -->
@@ -84,8 +87,10 @@ include "../../config.php";
                                         </thead>
 
                                         <tbody>
+
                                             <!-- Producto -->
                                             <tr>
+
                                                 <td class="text-end">1</td>
                                                 <td>
                                                     <div class="row">
@@ -98,7 +103,34 @@ include "../../config.php";
                                                 <td class="text-center">Nivel 1</td>
                                                 <td class="text-center">cliente@example.com</td>
                                                 <td class="text-center">Mensual</td>
+                                                <td class="text-center">
+                                                    <div class="prod-action-links">
+                                                        <ul class="list-inline me-auto mb-0">
+                                                            <!-- Ver orden -->
+                                                            <li class="list-inline-item align-bottom" data-bs-toggle="tooltip" title="View">
+                                                                <a href="#" class="avtar avtar-xs btn-link-secondary btn-pc-default" data-bs-toggle="offcanvas" data-bs-target="#productOffcanvas">
+                                                                    <i class="ti ti-eye f-18"></i>
+                                                                </a>
+                                                            </li>
+                                                            <!-- Editar orden -->
+                                                            <li class="list-inline-item align-bottom" data-bs-toggle="tooltip" title="Edit">
+                                                                <a href="ecom_product-add.html" class="avtar avtar-xs btn-link-success btn-pc-default">
+                                                                    <i class="ti ti-edit-circle f-18"></i>
+                                                                </a>
+                                                            </li>
+                                                            <!-- Eliminar orden -->
+                                                            <li class="list-inline-item align-bottom" data-bs-toggle="tooltip" title="Delete">
+                                                                <a href="#" class="avtar avtar-xs btn-link-danger btn-pc-default">
+                                                                    <i class="ti ti-trash f-18"></i>
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </td>
+
+
                                             </tr>
+
                                         </tbody>
                                     </table>
                                 </div>
@@ -109,85 +141,86 @@ include "../../config.php";
                 </div>
 
                 <!-- [ Main Content ] end -->
+            </div>
         </div>
-    </div>
 
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="productOffcanvas" aria-labelledby="productOffcanvasLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="productOffcanvasLabel">Product Details</h5>
-            <a href="#" class="avtar avtar-s btn-link-danger btn-pc-default" data-bs-dismiss="offcanvas">
-                <i class="ti ti-x f-20"></i>
-            </a>
-        </div>
-        <div class="offcanvas-body">
-            <div class="card product-card shadow-none border-0">
-                <div class="card-img-top p-0">
-                    <a href="ecom_product-details.html">
-                        <img src="../assets/images/application/img-prod-4.jpg" alt="image" class="img-prod img-fluid" />
-                    </a>
-                    <div class="card-body position-absolute end-0 top-0">
-                        <div class="form-check prod-likes">
-                            <input type="checkbox" class="form-check-input" />
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="feather feather-heart prod-likes-icon">
-                                <path
-                                    d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                            </svg>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="productOffcanvas" aria-labelledby="productOffcanvasLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="productOffcanvasLabel">Product Details</h5>
+                <a href="#" class="avtar avtar-s btn-link-danger btn-pc-default" data-bs-dismiss="offcanvas">
+                    <i class="ti ti-x f-20"></i>
+                </a>
+            </div>
+            <div class="offcanvas-body">
+                <div class="card product-card shadow-none border-0">
+                    <div class="card-img-top p-0">
+                        <a href="ecom_product-details.html">
+                            <img src="../assets/images/application/img-prod-4.jpg" alt="image" class="img-prod img-fluid" />
+                        </a>
+                        <div class="card-body position-absolute end-0 top-0">
+                            <div class="form-check prod-likes">
+                                <input type="checkbox" class="form-check-input" />
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="feather feather-heart prod-likes-icon">
+                                    <path
+                                        d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="card-body position-absolute start-0 top-0">
+                            <span class="badge bg-danger badge-prod-card">30%</span>
                         </div>
                     </div>
-                    <div class="card-body position-absolute start-0 top-0">
-                        <span class="badge bg-danger badge-prod-card">30%</span>
-                    </div>
                 </div>
+                <h5>Glitter gold Mesh Walking Shoes</h5>
+                <p class="text-muted">Image Enlargement: After shooting, you can enlarge photographs of the objects for clear zoomed view. Change In Aspect Ratio:
+                    Boldly crop the subject and save it with a composition that has impact.</p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item px-0">
+                        <div class="d-inline-flex align-items-center justify-content-between w-100">
+                            <p class="mb-0 text-muted me-1">Price</p>
+                            <h4 class="mb-0"><b>$299.00</b><span class="mx-2 f-14 text-muted f-w-400 text-decoration-line-through">$399.00</span></h4>
+                        </div>
+                    </li>
+                    <li class="list-group-item px-0">
+                        <div class="d-inline-flex align-items-center justify-content-between w-100">
+                            <p class="mb-0 text-muted me-1">Categories</p>
+                            <h6 class="mb-0">Shoes</h6>
+                        </div>
+                    </li>
+                    <li class="list-group-item px-0">
+                        <div class="d-inline-flex align-items-center justify-content-between w-100">
+                            <p class="mb-0 text-muted me-1">Status</p>
+                            <h6 class="mb-0"><span class="badge bg-warning rounded-pill">Process</span></h6>
+                        </div>
+                    </li>
+                    <li class="list-group-item px-0">
+                        <div class="d-inline-flex align-items-center justify-content-between w-100">
+                            <p class="mb-0 text-muted me-1">Brands</p>
+                            <h6 class="mb-0"><img src="../assets/images/application/img-prod-brand-1.png" alt="user-image" class="wid-40" /></h6>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <h5>Glitter gold Mesh Walking Shoes</h5>
-            <p class="text-muted">Image Enlargement: After shooting, you can enlarge photographs of the objects for clear zoomed view. Change In Aspect Ratio:
-                Boldly crop the subject and save it with a composition that has impact.</p>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item px-0">
-                    <div class="d-inline-flex align-items-center justify-content-between w-100">
-                        <p class="mb-0 text-muted me-1">Price</p>
-                        <h4 class="mb-0"><b>$299.00</b><span class="mx-2 f-14 text-muted f-w-400 text-decoration-line-through">$399.00</span></h4>
-                    </div>
-                </li>
-                <li class="list-group-item px-0">
-                    <div class="d-inline-flex align-items-center justify-content-between w-100">
-                        <p class="mb-0 text-muted me-1">Categories</p>
-                        <h6 class="mb-0">Shoes</h6>
-                    </div>
-                </li>
-                <li class="list-group-item px-0">
-                    <div class="d-inline-flex align-items-center justify-content-between w-100">
-                        <p class="mb-0 text-muted me-1">Status</p>
-                        <h6 class="mb-0"><span class="badge bg-warning rounded-pill">Process</span></h6>
-                    </div>
-                </li>
-                <li class="list-group-item px-0">
-                    <div class="d-inline-flex align-items-center justify-content-between w-100">
-                        <p class="mb-0 text-muted me-1">Brands</p>
-                        <h6 class="mb-0"><img src="../assets/images/application/img-prod-brand-1.png" alt="user-image" class="wid-40" /></h6>
-                    </div>
-                </li>
-            </ul>
         </div>
-    </div>
-    <!-- [ Main Content ] end -->
-    <?php include "../layouts/footer.php" ?>
-    <!-- Required Js -->
-    <?php include "../layouts/scripts.php" ?>
-    <!-- [Page Specific JS] start -->
-    <script src="../assets/js/plugins/simple-datatables.js"></script>
-    <!-- [Page Specific JS] end -->
-    <?php include "../layouts/modals.php" ?>
+        <!-- [ Main Content ] end -->
+        <?php include "../layouts/footer.php" ?>
+        <!-- Required Js -->
+        <?php include "../layouts/scripts.php" ?>
+        <!-- [Page Specific JS] start -->
+        <script src="../assets/js/plugins/simple-datatables.js"></script>
+        <!-- [Page Specific JS] end -->
+        <?php include "../layouts/modals.php" ?>
 </body>
 <!-- [Body] end -->
+
 </html>
