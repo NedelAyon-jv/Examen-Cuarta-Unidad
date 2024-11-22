9871dfc478e69e7c6419065b0fa8a7a8f5fd36e4
Vecs <166658809+Vecs09@users.noreply.github.com>
2024-11-22 00:50:08 -0700

    Backend / error en create product con el session start

--- app/BrandsController.php
+++ app/BrandsController.php
@@ -1,6 +1,7 @@
 <?php 
-session_start();
-include_once __DIR__ . '/../config.php';
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}include_once __DIR__ . '/../config.php';
 class BrandsController 
 {
 	
