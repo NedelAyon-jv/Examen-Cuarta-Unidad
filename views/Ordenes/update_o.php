d9b159e365ae1dad830b1d03072f780fe0f859be
NedelAyon-jv <tuola9@gmail.com>
2024-11-22 13:19:28 -0700

    Maquetado/modificación ordenes

--- views/Ordenes/update_o.php
+++ views/Ordenes/update_o.php
@@ -42,53 +42,63 @@ include_once "../../app/BrandsController.php";
             </div>
 
             <div class="row">
-                <form method="POST" action="../../app/ProductsController.php" enctype="multipart/form-data" class="w-100">
-                    <div class="col-xl-10">
-                        <div class="card w-100">
-                            <div class="card-header">
-                                <h5>Información de la orden</h5>
-                            </div>
-                            <div class="card-body">
-                                <div class="mb-3">
-                                    <label class="form-label">Folio:</label>
-                                    <input type="text" class="form-control w-100" id="name" name="name" placeholder="Ingresar folio" required />
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Total:</label>
-                                    <input type="text" class="form-control w-100" id="name" name="name" placeholder="Ingresar total" required />
-                                </div>
-                                <div class="mb-0">
-                                    <label class="form-label">Esta pagado:</label>
-                                    <textarea class="form-control w-100" id="description" name="description" placeholder="Esta pagado" required></textarea>
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Cliente Id</label>
-                                    <input type="text" class="form-control w-100" id="keywords" name="slug" placeholder="Cliente id" />
-                                </div>
-                                <div class="mb-0">
-                                    <label class="form-label">Direción id</label>
-                                    <textarea class="form-control w-100" id="features" name="features" placeholder="características" required></textarea>
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Estado de la orden id: </label>
-                                    <input type="text" class="form-control w-100" id="keywords" name="slug" placeholder="Estado de la orden" />
-                                </div>
-                                <div class="mb-0">
-                                    <label class="form-label">Tipo de pago id</label>
-                                    <textarea class="form-control w-100" id="features" name="features" placeholder="tipo de pago" required></textarea>
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Cupon Id</label>
-                                    <input type="text" class="form-control w-100" id="keywords" name="slug" placeholder="Cupon id" />
-                                </div>
-                                <div class="mb-0">
-                                    <label class="form-label">Cliente</label>
-                                    <textarea class="form-control w-100" id="features" name="features" placeholder="cliente" required></textarea>
-                                </div>
-                            </div>
+                <div class="col-xl-10">
+                        <div class="card-header">
+                            <h5>Información de la orden</h5>
                         </div>
-                    </div>
-                    <div class="col-sm-10">
+                        <div class="card-body">
+
+                            <div class="mb-3">
+                                <label class="form-label">Folio:</label>
+                                <input type="number" class="form-control w-100" id="name" name="folio" placeholder="Ingresar folio" required />
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Total:</label>
+                                <input type="number" class="form-control w-100" id="name" name="total" placeholder="Ingresar total" required />
+                            </div>
+                            <div class="mb-0">
+                                <label class="form-label">Esta pagado:</label>
+                                <textarea class="form-control w-100" id="description" name="is_paid" placeholder="Esta pagado" required></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Cliente Id</label>
+                                <input type="number" class="form-control w-100" id="keywords" name="client_id" placeholder="Cliente id" />
+                            </div>
+                            <div class="mb-0">
+                                <label class="form-label">Direción id</label>
+                                <textarea class="form-control w-100" id="features" name="address_id" placeholder="características" required></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Estado de la orden id: </label>
+                                <input type="number" class="form-control w-100" id="keywords" name="order_status_id" placeholder="Estado de la orden" />
+                            </div>
+                            <div class="mb-0">
+                                <label class="form-label">Tipo de pago id</label>
+                                <textarea class="form-control w-100" id="features" name="payment_type_id" placeholder="tipo de pago" required></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Cupon Id</label>
+                                <input type="number" class="form-control w-100" id="keywords" name="cupon_id" placeholder="Cupon id" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="presentations" class="form-label">Presentaciones</label>
+                                <div id="presentations">
+                                    <div class="presentation">
+                                        <label for="presentations[0][id]" class="form-label">ID Presentación 1</label>
+                                        <input type="text" class="form-control" name="presentations[0][id]" placeholder="ID Presentación 1" required>
+                                        <label for="presentations[0][quantity]" class="form-label">Cantidad Presentación 1</label>
+                                        <input type="number" class="form-control" name="presentations[0][quantity]" placeholder="Cantidad Presentación 1" required>
+                                    </div>
+                                    <div class="presentation">
+                                        <label for="presentations[1][id]" class="form-label">ID Presentación 2</label>
+                                        <input type="text" class="form-control" name="presentations[1][id]" placeholder="ID Presentación 2" required>
+                                        <label for="presentations[1][quantity]" class="form-label">Cantidad Presentación 2</label>
+                                        <input type="number" class="form-control" name="presentations[1][quantity]" placeholder="Cantidad Presentación 2" required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-10">
                         <div class="card">
                             <div class="card-body text-end btn-page">
                                 <button type="submit" class="btn btn-primary mb-0">Modificar orden </button>
@@ -97,8 +107,9 @@ include_once "../../app/BrandsController.php";
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 
